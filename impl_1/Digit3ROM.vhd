e10f5e39f44b62e07656ef4613c05ea67426bb78
Isaac Medina <isaac.josemedina@gmail.com>
2024-12-10 22:01:38 -0500

    Update Digit3ROM.vhd

--- impl_1/Digit3ROM.vhd
+++ impl_1/Digit3ROM.vhd
@@ -4,49 +4,37 @@ use IEEE.numeric_std.all;
 
 
 entity Digit3ROM is
-port(
-	Xin : in unsigned(4 downto 0);
-	Yin : in unsigned(4 downto 0);
-data : out std_logic_vector(5 downto 0)
-);
+	port(
+		Xin : in unsigned(4 downto 0);
+		Yin : in unsigned(4 downto 0);
+		data : out std_logic_vector(5 downto 0)
+	);
 end Digit3ROM;
 
 
 architecture synth of Digit3ROM is
+	signal addy : unsigned(9 downto 0);
+begin
 
+	addy <= Yin & Xin;
 
-Signal addy : unsigned(9 downto 0);
-
-
-Begin
-
-
-addy <= Yin & Xin;
-
-
-process(addy) begin
-	case addy is
-	when "0000000001" => data <= "000000";
-when "0000000010" => data <= "000000";
-when "0000100000" => data <= "000000";
-when "0000100011" => data <= "000000";
-when "0001000011" => data <= "000000";
-when "0001100010" => data <= "000000";
-when "0010000011" => data <= "000000";
-when "0010100000" => data <= "000000";
-when "0010100011" => data <= "000000";
-when "0011000001" => data <= "000000";
-when "0011000010" => data <= "000000";
-
-
-
-
-
-
-		when others => data <= "111111";
-end case;
-end process;
-end;
+	process(addy) begin
+		case addy is
+			when "0000000001" => data <= "000000";
+			when "0000000010" => data <= "000000";
+			when "0000100000" => data <= "000000";
+			when "0000100011" => data <= "000000";
+			when "0001000011" => data <= "000000";
+			when "0001100010" => data <= "000000";
+			when "0010000011" => data <= "000000";
+			when "0010100000" => data <= "000000";
+			when "0010100011" => data <= "000000";
+			when "0011000001" => data <= "000000";
+			when "0011000010" => data <= "000000";
+			when others => data <= "111111";
+		end case;
+	end process;
+end synth;
 
 
 
