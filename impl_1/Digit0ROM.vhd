1a4089881bb3da8f0c5d9bf594d6a4a4f31eb6f3
Isaac Medina <isaac.josemedina@gmail.com>
2024-12-10 21:58:42 -0500

    Update Digit0ROM.vhd

--- impl_1/Digit0ROM.vhd
+++ impl_1/Digit0ROM.vhd
@@ -14,40 +14,34 @@ end entity;
 
 architecture synth of Digit0ROM is
 
-
-Signal addy : unsigned(9 downto 0);
-
-
-Begin
-
-
-addy <= Yin & Xin;
-
-
-process(addy) begin
-	case addy is
-		when "0000000000" => data <= "000000";
-		when "0000000001" => data <= "000000";
-		when "0000000010" => data <= "000000";
-		when "0000000011" => data <= "000000";
-		when "0000100000" => data <= "000000";
-		when "0000100011" => data <= "000000";
-		when "0001000000" => data <= "000000";
-		when "0001000011" => data <= "000000";
-		when "0001100000" => data <= "000000";
-		when "0001100011" => data <= "000000";
-		when "0010000000" => data <= "000000";
-		when "0010000011" => data <= "000000";
-		when "0010100000" => data <= "000000";
-		when "0010100011" => data <= "000000";
-		when "0011000000" => data <= "000000";
-		when "0011000001" => data <= "000000";
-		when "0011000010" => data <= "000000";
-		when "0011000011" => data <= "000000";
-		when others => data <= "111111";
-end case;
-end process;
-end;
+	signal addy : unsigned(9 downto 0);
+	
+begin
+	addy <= Yin & Xin;
+	process(addy) begin
+		case addy is
+			when "0000000000" => data <= "000000";
+			when "0000000001" => data <= "000000";
+			when "0000000010" => data <= "000000";
+			when "0000000011" => data <= "000000";
+			when "0000100000" => data <= "000000";
+			when "0000100011" => data <= "000000";
+			when "0001000000" => data <= "000000";
+			when "0001000011" => data <= "000000";
+			when "0001100000" => data <= "000000";
+			when "0001100011" => data <= "000000";
+			when "0010000000" => data <= "000000";
+			when "0010000011" => data <= "000000";
+			when "0010100000" => data <= "000000";
+			when "0010100011" => data <= "000000";
+			when "0011000000" => data <= "000000";
+			when "0011000001" => data <= "000000";
+			when "0011000010" => data <= "000000";
+			when "0011000011" => data <= "000000";
+			when others => data <= "111111";
+		end case;
+	end process;
+end synth;
 
 
 
